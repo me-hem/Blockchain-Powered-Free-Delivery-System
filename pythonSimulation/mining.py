c64bce870b3e3c77ddc9695e6e16c3bc5abec2c3
me-hem <umesh.bhatt2002@gmail.com>
2023-10-08 17:14:22 +0530

    Ingestion ongoing code working fine

--- pythonSimulation/mining.py
+++ pythonSimulation/mining.py
@@ -7,10 +7,10 @@ print("Mining started ")
 count = 0
 time.sleep(1)
 while count < 3:
-    time.sleep(20)
     res = req.get(url)
     print(res.content.decode('utf-8'))
     if res.content.decode('utf-8') == "No transactions to mine":
         count += 1
+    time.sleep(5)
 
 print("Mining completed successfully")
