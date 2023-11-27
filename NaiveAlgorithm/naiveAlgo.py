3335a67e20f46391550f16069ded5ace9f63877c
Umesh Bhatt <103458788+me-hem@users.noreply.github.com>
2023-11-27 18:27:42 +0530

    Update naiveAlgo.py

--- NaiveAlgorithm/naiveAlgo.py
+++ NaiveAlgorithm/naiveAlgo.py
@@ -1,8 +1,10 @@
 import json
 import pandas as pd
+import time
 
 df = pd.read_csv("/home/ethereum/Downloads/ResearchData/data/order500k.csv")
 
+start_time = time.perf_counter()
 threshold = 200000
 
 free_delivery = []
@@ -15,3 +17,14 @@ for i in range(df.shape[0]):
 df["free_delivery"] = free_delivery
 
 df.to_csv("naiveReward500k.csv")
+
+end_time = time.perf_counter()
+
+processing_time = end_time - start_time
+with open("log.json") as file:
+    data = json.load(file)
+    
+data["100K - naive"] = processing_time
+
+with open("log.json", w) as file:
+    json.dump(data, file)
