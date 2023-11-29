82d9438b4800d15e36ba69b9b380121f7dde5944
me-hem <umesh.bhatt2002@gmail.com>
2023-11-29 19:44:13 +0530

    backtracking logs added

--- HIShipAlgorithm/HIShip.py
+++ HIShipAlgorithm/HIShip.py
@@ -10,10 +10,10 @@ start_time = time.perf_counter()
 column = []
 for i in range(df.shape[0]):
     column.append("no")
-print(column)
+
 
 df["o_freedelivery"] = column  # Add the new column to the DataFrame
-df.head()
+
 
 # Function to check if free delivery is applicable based on total spent
 def check_freedelivery(total_spent, o_totalprice):
@@ -26,6 +26,7 @@ def check_freedelivery(total_spent, o_totalprice):
 # Function to calculate total spent by a customer before a specific order date
 def calc_total(o_custkey, o_orderdate, chain):
     total_spent = 0
+    backtrack_count = 0
     for i in range(chain.shape[0]-1, -1, -1):
         tx_date = chain.iloc[i, 3]
         spent_time = int(o_orderdate[:4]) - int(tx_date[:4]) + int(o_orderdate[5:7])/12 - int(tx_date[5:7])/12 + int(o_orderdate[8:])/365 - int(tx_date[8:])/365
@@ -39,23 +40,31 @@ def calc_total(o_custkey, o_orderdate, chain):
                 if tx_freedelivery == "no":
                     total_spent += tx_price
                 else:
-                    return total_spent
+                    return total_spent, backtrack_count
         else:
-            return total_spent
-    return total_spent
+            return total_spent, backtrack_count
+        backtrack_count += 1
+    return total_spent, backtrack_count
+
+with open("backtrack_log10K.json") as back_file:
+    backtrack_log = json.load(back_file)
 
 # Loop through each row in the DataFrame to calculate total spent and check free delivery eligibility
 for tx_no in range(df.shape[0]):
-    total_spent = calc_total(df.iloc[tx_no, 1], df.iloc[tx_no, 3], df.iloc[:tx_no])
+    total_spent, backtrack_count = calc_total(df.iloc[tx_no, 1], df.iloc[tx_no, 3], df.iloc[:tx_no])
     print(tx_no, ":", total_spent, end="=>")
     print(check_freedelivery(total_spent, df.iloc[tx_no, 2]))
     df.iloc[tx_no, 4] = check_freedelivery(total_spent, df.iloc[tx_no, 2])
+    backtrack_log[str(df.iloc[tx_no, 1])] = str(backtrack_count)
 
 # Save the DataFrame to a CSV file
 df.to_csv("chainReward10K.csv")
 
-end_time = time.perf_counter()
 
+with open("backtrack_log10K.json", 'w') as back_file:
+    json.dump(backtrack_log, back_file)
+
+end_time = time.perf_counter()
 processing_time = end_time - start_time
 with open("log.json") as file:
     data = json.load(file)
@@ -65,3 +74,4 @@ data["10K - HIShip"] = processing_time
 # Save the updated log data to the JSON file
 with open("log.json", 'w') as file:
     json.dump(data, file)
+
