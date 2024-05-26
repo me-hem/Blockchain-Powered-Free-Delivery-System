ca2a6227ce6a194b3252eda504825964f73e2e62
Umesh Bhatt <103458788+me-hem@users.noreply.github.com>
2024-05-26 14:35:29 +0530

    recommendation code added

--- HIShipAlgorithm/HIShip.py
+++ HIShipAlgorithm/HIShip.py
@@ -24,9 +24,30 @@ def check_freedelivery(total_spent, o_totalprice):
         return "no"
 
 # Function to calculate total spent by a customer before a specific order date
+# def calc_total(o_custkey, o_orderdate, chain):
+#     total_spent = 0
+#     backtrack_count = 0
+#     for i in range(chain.shape[0]-1, -1, -1):
+#         tx_date = chain.iloc[i, 3]
+#         spent_time = int(o_orderdate[:4]) - int(tx_date[:4]) + int(o_orderdate[5:7])/12 - int(tx_date[5:7])/12 + int(o_orderdate[8:])/365 - int(tx_date[8:])/365
+#         tx_price = round(chain.iloc[i, 2])
+#         tx_custkey = chain.iloc[i, 1]
+#         tx_freedelivery = chain.iloc[i, 4]
+        
+#         # Check if the transaction is within 1 year and matches the customer and does not have free delivery
+#         if spent_time <= 1:
+#             if o_custkey == tx_custkey:
+#                 if tx_freedelivery == "no":
+#                     total_spent += tx_price
+#                 else:
+#                     return total_spent, backtrack_count
+#         else:
+#             return total_spent, backtrack_count
+#         backtrack_count += 1
+#     return total_spent, backtrack_count
+
 def calc_total(o_custkey, o_orderdate, chain):
     total_spent = 0
-    backtrack_count = 0
     for i in range(chain.shape[0]-1, -1, -1):
         tx_date = chain.iloc[i, 3]
         spent_time = int(o_orderdate[:4]) - int(tx_date[:4]) + int(o_orderdate[5:7])/12 - int(tx_date[5:7])/12 + int(o_orderdate[8:])/365 - int(tx_date[8:])/365
@@ -40,28 +61,30 @@ def calc_total(o_custkey, o_orderdate, chain):
                 if tx_freedelivery == "no":
                     total_spent += tx_price
                 else:
-                    return total_spent, backtrack_count
+                    return total_spent
         else:
-            return total_spent, backtrack_count
-        backtrack_count += 1
-    return total_spent, backtrack_count
+            return total_spent
+    return total_spent
 
-with open("backtrack_log10K.json") as back_file:
-    backtrack_log = json.load(back_file)
+# with open("backtrack_log10K.json") as back_file:
+#     backtrack_log = json.load(back_file)
 
 # Loop through each row in the DataFrame to calculate total spent and check free delivery eligibility
 for tx_no in range(df.shape[0]):
-    total_spent, backtrack_count = calc_total(df.iloc[tx_no, 1], df.iloc[tx_no, 3], df.iloc[:tx_no])
-    print(tx_no+1, ":", total_spent, "=>", check_freedelivery(total_spent, df.iloc[tx_no, 2]), ":", backtrack_count)
+    # total_spent, backtrack_count = calc_total(df.iloc[tx_no, 1], df.iloc[tx_no, 3], df.iloc[:tx_no])
+    # print(tx_no+1, ":", total_spent, "=>", check_freedelivery(total_spent, df.iloc[tx_no, 2]), ":", backtrack_count)
+    # df.iloc[tx_no, 4] = check_freedelivery(total_spent, df.iloc[tx_no, 2])
+    # backtrack_log[str(tx_no+1)] = str(backtrack_count)
+    total_spent = calc_total(df.iloc[tx_no, 1], df.iloc[tx_no, 3], df.iloc[:tx_no])
+    print(tx_no+1, ":", total_spent, "=>", check_freedelivery(total_spent, df.iloc[tx_no, 2]))
     df.iloc[tx_no, 4] = check_freedelivery(total_spent, df.iloc[tx_no, 2])
-    backtrack_log[str(tx_no+1)] = str(backtrack_count)
 
 # Save the DataFrame to a CSV file
 df.to_csv("chainReward10K.csv")
 
 
-with open("backtrack_log10K.json", 'w') as back_file:
-    json.dump(backtrack_log, back_file)
+# with open("backtrack_log10K.json", 'w') as back_file:
+#     json.dump(backtrack_log, back_file)
 
 end_time = time.perf_counter()
 processing_time = end_time - start_time
